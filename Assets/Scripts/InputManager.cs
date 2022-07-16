f017011976544b227df56b0fc89508d0551c6220
Hyoin Jung <68385607+hyoinandout@users.noreply.github.com>
2022-07-17 06:23:11 +0900

    feat:Add LightManager

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,15 +8,17 @@ public class InputManager : MonoBehaviour
     SerialPort arduino = new SerialPort("COM3", 9600);
     private string inputData;
     private string[] splitedInputData;
+    private string[] splitedTempMoist;
     private string inputdataType;
     
     public float light;
     public float moisture;
+    public float temperature;
     public float dirtMoisture;
     public float distance_1;
     public Vector3 currentPosition;
 
-    //public Button panel;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,23 +29,28 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*splitedInputData = inputData.Split(':');
+        inputData = arduino.ReadLine();
+        splitedInputData = inputData.Split(':');
 
-        if(splitedInputData[0] == "1") //조도
+        if (splitedInputData[0] == "3") //조도
         {
-            light=float.Parse(splitedInputData[1]);
+            light = float.Parse(splitedInputData[1]);
+            Debug.Log(light);
         }
-        else if(splitedInputData[0]=="1") //토양습도
+        else if (splitedInputData[0] == "1") //토양습도
         {
-            dirtMoisture=float.Parse(splitedInputData[1]);
+            dirtMoisture = float.Parse(splitedInputData[1]);
+            Debug.Log(dirtMoisture);
         }
-        else if(splitedInputData[0]=="2") //습도
+        else if (splitedInputData[0] == "2") //습도
         {
-            moisture=float.Parse(splitedInputData[1]);
+            /*splitedTempMoist = splitedInputData.Split(' ');
+            moisture = float.Parse(splitedTempMoist[0]);
+            temperature = float.Parse(splitedTempMoist[1]);*/
         }
-        else if(splitedInputData[0]=="4") //초음파1
+        else if (splitedInputData[0] == "4") //초음파1
         {
-            distance_1=float.Parse(splitedInputData[1]);
-        }*/
+            distance_1 = float.Parse(splitedInputData[1]);
+        }
     }
 } 
--- Assets/Scripts/LightManager.cs
+++ Assets/Scripts/LightManager.cs
@@ -21,11 +21,13 @@ public class LightManager : MonoBehaviour
     }
     private void Update()
     {
+        intensity = (byte)inputManager.light;
         tempLight.r = intensity;
         tempLight.g = intensity;
         tempLight.b = intensity;
         roomLight.color = tempLight;
         backgroundImage.color = tempLight;
     }
-
+    // 조도에 따라서 표정 변화도 있음.
+    // 이마에 해 붙이고 있는 애가 일정 조도 이하로 내려가면 우울해진다.
 }
